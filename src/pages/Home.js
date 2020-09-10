bdd207c08eaa1f668246c90d6d940450b2ed260d
dangn511 <41414735+dangn511@users.noreply.github.com>
2020-09-10 11:24:59 +0800

    update for orientation day treasures

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,6 +20,10 @@ const HomeComp = () => {
         Java Edition 1.15.2
         <br></br>
         When entering the server, please use the portal on the left to teleport to the Open House world.
+        <br></br>
+        Found some posters around school with info about treasures? Head into the Open House world and start searching! There are chests hidden around the in-game counterpart of where you spotted the posters.
+        <br></br>
+        If you have found a chest, follow the instructions written in the in-game book to redeem your prize. Stand to win Starbucks vouchers, and special prizes for the first 3 winners.
       </Text>
       <CTA href="https://sutdmc-map.opensutd.org/open_house">Live 3D Maps (View in Browser)</CTA>
     </PageBase>
