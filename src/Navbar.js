810055aef724a6121b141a218830f01a3a15398b
ltiinagn <lingtianmsnexplorer@msn.com>
2021-02-28 00:48:30 +0800

    test add navbar dropdowns

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import Link from "./components/Link";
+import "./Navbar.css"
 
 const NavLink = styled(Link)``;
 
@@ -26,6 +27,16 @@ const Navbar = styled.ul`
     padding-bottom: 5%;
     padding-left: 15%;
     padding-right: 15%;
+    &:hover {
+      color: var(--color-accent);
+    }
+
+    &::selection {
+      background-color: var(--color-accent);
+      color: var(--color-background);
+    }
+  }
+  & > li > div > a {
     &:hover {
       color: var(--color-accent);
     }
@@ -61,6 +72,22 @@ function NavbarComp() {
         </li>
         <li>
           <NavLink to="/about">About</NavLink>
+        </li>
+        <li className="nav-dropdown">
+          <NavLink to="/games">Games</NavLink>
+          <div class="nav-dropdown-content">
+            <NavLink to="/epd">EPD</NavLink>
+            <NavLink to="/esd">ESD</NavLink>
+            <NavLink to="/istd">ISTD</NavLink>
+          </div>
+        </li>
+        <li className="nav-dropdown">
+          <NavLink to="/projects">Projects</NavLink>
+          <div class="nav-dropdown-content">
+            <NavLink to="/project1">Project&nbsp;1</NavLink>
+            <NavLink to="/project2">Project&nbsp;2</NavLink>
+            <NavLink to="/project3">Project&nbsp;3</NavLink>
+          </div>
         </li>
         <li>
           <a href="https://opensutd.org/minecraft-gallery/">Gallery</a>
