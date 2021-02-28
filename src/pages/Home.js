79976ae179013540872626cdbb20b7c1e61d2d0c
Joel <joeltwh1999@gmail.com>
2021-02-28 10:33:50 +0800

    removed previous implementation of buttons, added them to home page instead

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,55 +1,74 @@
 import React, {useState} from "react";
+import styled from "styled-components";
 
 import PageBase from "../components/PageBase";
 import PageHeader from "../components/PageHeader";
 // import SubHeader from "../components/SubHeader";
 import Text from "../components/Text";
 import CTA from "../components/CTA";
+import CTAbtn from '../components/CTAbtn';
 
-import HomeSelect from './HomeSelect';
+const BtnGroup = styled.div`
+  display: flex;
+
+  & > * {
+    margin-right: 10px;
+    margin-top: 0;
+  }
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+
+    & > * {
+      margin-top: 10px;
+    }
+  }
+`;
 
 const HomeComp = () => {
   let userHadSession = false;
   //let userType; //a string, either 'minecraft' or 'nonMinecraft'
-  const [userType, setUserType] = useState('');
+  const [userType, setUserType] = useState('minecraft');
 
   const selectUserType = e => {
     let selectedType;
-    if (e.target.innerText === 'I PLAYED MINECRAFT') {
+    if (e.target.innerText === 'I PLAY MINECRAFT') {
       selectedType = 'minecraft';
     } else {
       selectedType = 'nonMinecraft';
     }
     setUserType(selectedType);
-    localStorage.setItem('user', selectedType);
-    window.location.reload();
   };
 
   return (
     <PageBase>
-      {localStorage.getItem('user') ? 
-      <PageBase>
-          <PageHeader>Welcome!</PageHeader>
-          <Text>
-            Take a tour of a blocky and virtual rendition of SUTD in a
-            student-hosted Minecraft server. Virtual Open Campus is open day or night, rain or shine!
-          </Text>
-          <Text>
-            Address: <code>game.sutdmc.opensutd.org</code>
-            <br></br>
-            Java Edition 1.15.2
-            <br></br>
-            When entering the server, please use the portal on the right to teleport to the Open House world.
-          </Text>
-          <Text>
-            Found some posters around school with info about treasures? Head into the Open House world and start searching! There are chests hidden around the in-game counterpart of where you spotted the posters.
-            <br></br>
-            If you have found a chest, follow the instructions written in the in-game book to redeem your prize. Stand to win Starbucks vouchers, and special prizes for the first 3 winners.
-          </Text>
-          <CTA href="https://sutdmc-map.opensutd.org/open_house">Live 3D Maps (View in Browser)</CTA>
-      </PageBase>
-      : <HomeSelect selectUserType={selectUserType}/>}
-
+      <PageHeader>Welcome!</PageHeader>
+      <BtnGroup>
+        <CTAbtn selectUserType={selectUserType}>I play Minecraft</CTAbtn>
+        <CTAbtn selectUserType={selectUserType}>I don't play minecraft</CTAbtn>
+      </BtnGroup>
+      {userType === 'minecraft' ?
+      <React.Fragment>
+        <Text>
+        Take a tour of a blocky and virtual rendition of SUTD in a
+        student-hosted Minecraft server. Virtual Open Campus is open day or night, rain or shine!
+        </Text>
+        <Text>
+          Address: <code>game.sutdmc.opensutd.org</code>
+          <br></br>
+          Java Edition 1.15.2
+          <br></br>
+          When entering the server, please use the portal on the right to teleport to the Open House world.
+        </Text>
+        <Text>
+          Found some posters around school with info about treasures? Head into the Open House world and start searching! There are chests hidden around the in-game counterpart of where you spotted the posters.
+          <br></br>
+          If you have found a chest, follow the instructions written in the in-game book to redeem your prize. Stand to win Starbucks vouchers, and special prizes for the first 3 winners.
+        </Text>
+        <CTA href="https://sutdmc-map.opensutd.org/open_house">Live 3D Maps (View in Browser)</CTA>
+      </React.Fragment> :
+      <Text>Non minecraft player</Text>
+      }
       
     </PageBase>
   );
