8b52ed562b4b90578befba4970b98d1e49967aca
ltiinagn <lingtianmsnexplorer@msn.com>
2021-03-04 22:19:16 +0800

    text formatting for cards and section text

--- src/pages/OH2021.js
+++ src/pages/OH2021.js
@@ -19,13 +19,13 @@ const gameCardDetails = {
         title: 'Redstone Gauntlet',
         href: '',
         imgSource: require('../assets/images/oh2021-games/redstone_rush.png'),
-        description: 'Test your Boolean logic as fast as possible in our ISTD inspired game and complete the gauntlet!' 
+        description: 'Test your Boolean logic as fast as possible in our ISTD inspired game and complete the gauntlet!'
     },
     gallery: {
         title: 'Speed Bakers',
         href: '',
         imgSource: require('../assets/images/oh2021-games/speed_bakers.png'),
-        description: 'Optimise your very own cake supply chain in our ESD inspired game!' 
+        description: 'Optimise your very own cake supply chain in our ESD inspired game!'
     }
 };
 
@@ -38,13 +38,17 @@ const sectionHeaders = {
     'margin': '40px auto 20px auto',
 }
 
+const sectionText = {
+    'text-align': 'center'
+}
+
 const OH2021Comp = () => {
     return (
         <PageBase>
             <PageHeader>Open House 2021</PageHeader>
             <PageBanner></PageBanner>
             <Text style={sectionHeaders}>Games</Text>
-            <Text style={sectionHeaders}>Starbuck, GV and Gong Cha vouchers to be won, first come first serve!</Text>
+            <Text style={sectionText}>Starbucks, GV and Gong Cha vouchers to be won, first come first serve!</Text>
             <FeatureCardSection cardDetails={gameCardDetails}></FeatureCardSection>
             <Text style={sectionHeaders}>View our ASD Projects</Text>
             <CTA href="/asd2020" style={ASDBtnStyles}>ASD Projects</CTA>
